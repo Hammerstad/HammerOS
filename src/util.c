4f1a03b3fd50df33e129a69ca418105c09497d11
Eirik Mildestveit Hammerstad <emhammerstad@gmail.com>
2015-07-11 22:04:46 +0200

    Added memcpy and memset.

--- src/util.c
+++ src/util.c
@@ -1,6 +1,7 @@
 #include "util.h"
 
-char* itoa(int i, char b[]){
+char* itoa(int i, char b[])
+{
     char const digit[] = "0123456789";
     char* p = b;
     if(i<0){
@@ -18,4 +19,17 @@ char* itoa(int i, char b[]){
         i = i/10;
     } while(i);
     return b;
+}
+
+void memcpy(uint8_t *destination, const uint8_t *src, uint32_t len)
+{
+    const uint8_t *start_pointer = (const uint8_t *)src;
+    uint8_t *destination_pointer = (uint8_t *)destination;
+    while(len-- != 0) *destination_pointer++ = *start_pointer++;
+}
+
+void memset(uint8_t *destination, uint8_t val, uint32_t len)
+{
+    uint8_t *temp = (uint8_t *)destination;
+    while (len-- != 0) *temp++ = val;
 }
--- src/util.h
+++ src/util.h
@@ -1,6 +1,10 @@
 #ifndef UTIL_H
 #define	UTIL_H
 
+#include <stdint.h>
+
 char* itoa(int i, char b[]);
+void memcpy(uint8_t *destination, const uint8_t *src, uint32_t len);
+void memset(uint8_t *destination, uint8_t val, uint32_t len);
 
 #endif	/* UTIL_H */
